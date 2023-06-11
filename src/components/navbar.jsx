438a27d79b950e9a330cd33894a4699723e81b0d
shashwath63 <shashwathbn10@gmail.com>
2023-06-11 16:58:54 +0530

    project frontend

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,6 +7,7 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="links">
         <Link to="/">Shop</Link>
+        <Link to="/contact">Contact</Link>
         <Link to="/cart">
        <ShoppingCart size={32}/>
         </Link>
